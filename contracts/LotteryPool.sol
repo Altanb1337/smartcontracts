0b36a755f50bdf627cd3ddcb6449ce319fd7ddce
Abalon <Abalon>
2021-04-18 17:33:56 +0200

    Project info comment

--- contracts/LotteryPool.sol
+++ contracts/LotteryPool.sol
@@ -1,3 +1,9 @@
+/*
+ * onepool.finance
+ * Yield Farming/Lottery
+ *
+ * https://t.me/onepoolfinance
+ */
 pragma solidity >=0.6.0<0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
--- contracts/PoolMaster.sol
+++ contracts/PoolMaster.sol
@@ -1,5 +1,10 @@
+/*
+ * onepool.finance
+ * Yield Farming/Lottery
+ *
+ * https://t.me/onepoolfinance
+ */
 pragma solidity >=0.6.0<0.8.0;
-
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
